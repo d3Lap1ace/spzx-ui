dd02787255bb997ac3d3bb4387859e9a1e19dbce
d3lap1ace L.Lyu <ap1cem@163.com>
2024-07-06 16:54:26 +0800

    init

---
+++ spzx-ui/src/api/product/brand.js
@@ -0,0 +1,61 @@
+import request from '@/utils/request'
+
+export function listBrand(query) {
+    return request({
+        url: '/product/brand/list',
+        method: 'get',
+        params: query
+    })
+
+}
+
+// 新增品牌
+export function addBrand(data) {
+    return request({
+      url: '/product/brand',
+      method: 'post',
+      data: data
+    })
+}
+
+
+// 查询品牌详细
+export function getBrand(id) {
+    return request({
+      url: '/product/brand/' + id,
+      method: 'get'
+    })
+}
+  
+  // 修改品牌
+export function updateBrand(data) {
+    return request({
+        url: '/product/brand',
+        method: 'put',
+        data: data
+    })
+}
+
+
+// 删除品牌
+export function delBrand(id) {
+    return request({
+      url: '/product/brand/' + id,
+      method: 'delete'
+    })
+}
+
+const brandList = ref([
+    {
+        createTime: "2024-01-09 10:15:05",
+        id: 1,
+        name: "华为",
+        logo: "http://139.198.127.41:9000/sph/20230506/华为.png",
+    },
+    {
+        createTime: "2024-01-19 10:15:05",
+        id: 2,
+        name: "小米",
+        logo: "http://139.198.127.41:9000/sph/20240202/xm.png",
+    },
+]);
